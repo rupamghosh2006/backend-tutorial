fc61c5df3e830ad360342224d6baea0b43f7346a
rupamghosh2006 <rupamgh2006@gmail.com>
2025-07-07 17:47:40 +0530

    util codes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
 // require('dotenv').config({path: './env'})
-import dotevn from "dotenv"
+import dotenv from "dotenv"
 // import mongoose from "mongoose";
 // import { DB_NAME } from "./constants";
 import connectDB from "./db/index.js";
+
+import { app } from "./app.js";
 
 dotevn.config({
     path: './env'
 })
 
 connectDB()
-
+.then(() => {
+    app.on("error", (error) => {
+            console.error("ERROR: ", error)
+            throw error
+    })
+    app.listen(process.env.PORT || 8000, () =>{
+        console.log(`Server is running at port :
+            ${process.env.PORT}`);        
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!!", err);    
+})
 
 
 
